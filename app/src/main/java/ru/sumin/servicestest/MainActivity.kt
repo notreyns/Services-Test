b889e34932e0b406d1f60702124c00c7c42f278c
Reina Khasanova <reina.khasanova@iaau.edu.kg>
2022-01-20 13:14:37 +0600

    notification manager

--- app/src/main/java/ru/sumin/servicestest/MainActivity.kt
+++ app/src/main/java/ru/sumin/servicestest/MainActivity.kt
@@ -1,11 +1,16 @@
 package ru.sumin.servicestest
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
 import ru.sumin.servicestest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private var id = 0
     private val binding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
@@ -13,8 +18,35 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        binding.simpleService.setOnClickListener{
+        binding.simpleService.setOnClickListener {
             startService(MyService.newInstance(this))
         }
+        binding.foregroundService.setOnClickListener {
+            showNotification()
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNLE_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            notificationManager.createNotificationChannel(channel)
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("text")
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .build()
+        notificationManager.notify(id++, notification)
+    }
+
+    companion object {
+        const val CHANNEL_ID = "channel_id"
+        const val CHANNLE_NAME = "channel_name"
     }
 }
